07f12ca023f6294a95166ad03039fae03274349b
Morgan Rees <morgan_rees@live.co.uk>
2026-01-09 19:52:04 +0000

    Create new angular project for rebuild

---
+++ DisJockey.Client/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+	{
+		path: '',
+		component: Home
+	}
+];
---
+++ DisJockey.Client/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
